863246fec1a94fd458fa5bcbd047a4e15505db18
apolloi52 <apolloivanovic@gmail.com>
2025-10-11 10:33:37 -0400

    smth

--- Assets/Hands-On Homework #2/Scripts/HW2PlayerRotation.cs
+++ Assets/Hands-On Homework #2/Scripts/HW2PlayerRotation.cs
@@ -2,5 +2,21 @@ using UnityEngine;
 
 public class HW2PlayerRotation : MonoBehaviour
 {
+    private Camera _camera;
 
+    private string CameraName = ("GameCamera");
+
+    private Vector3 _PositionMouse;
+
+    private void Start()
+    {
+     _camera = GameObject.Find(CameraName).GetComponent<Camera>();   
+    }
+
+    private void Update()
+    {
+        _PositionMouse = _camera.ScreenToWorldPoint(Input.mousePosition);
+
+        Vector3 pos = _PositionMouse - transform.position;
+    }
 }
