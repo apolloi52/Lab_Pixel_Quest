c7dafb27d321e9e535c87a29785ad742f2a7af4a
apolloi52 <apolloivanovic@gmail.com>
2025-11-01 12:13:08 -0400

    rrr

--- Assets/Pixel_Quest/Scripts/PlayerJump.cs
+++ Assets/Pixel_Quest/Scripts/PlayerJump.cs
@@ -4,15 +4,56 @@ using UnityEngine;
 
 public class PlayerJump : MonoBehaviour
 {
+    public float jumpForce = 10f;
+    private Rigidbody2D rb;
+
+    public float CapsuleHeight = 0.2f;
+    public float CapsuleRadius = 0.05f;
+    private bool groundcheck;
+    public Transform feetCollider;
+    public LayerMask groundLayer;
+
+    public float fallforce = 1.5f;
+    Vector2 gravityvector;
+
+    private bool waterCheck = false;
+    private string waterTag = "Water"; 
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        gravityvector = new Vector2 (0, Physics2D.gravity.y);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        groundcheck = (Physics2D.OverlapCapsule(feetCollider.position, new Vector2(CapsuleHeight, CapsuleRadius), CapsuleDirection2D.Horizontal, 0, groundLayer));
+
+        if ((groundcheck || waterCheck) && Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        }
+
+        if (rb.velocity.y < 0 && !waterCheck)
+        {
+            rb.velocity += gravityvector * fallforce * 1/2*Time.deltaTime;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.CompareTag(waterTag))
+        {
+            waterCheck = true;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag(waterTag))
+        {
+            waterCheck = false;
+        }
     }
 }
--- Assets/Pixel_Quest/Scripts/PlayerMove.cs
+++ Assets/Pixel_Quest/Scripts/PlayerMove.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Tilemaps;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -7,11 +8,17 @@ public class PlayerMove : MonoBehaviour
 {
     private Rigidbody2D rb;
     public float Speed = 5;
+    private SpriteRenderer spriteRenderer;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+
+        if (spriteRenderer = transform.GetChild(0).GetComponent<SpriteRenderer>())
+        {
+            spriteRenderer = transform.GetChild(0).GetComponent<SpriteRenderer>();
+        }
     }
 
     // Update is called once per frame
@@ -29,6 +36,14 @@ public class PlayerMove : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.W))
         {
             rb.velocity += new Vector2(rb.velocity.x, 1);
+        }
+        if (xInput > 0)
+        {
+            spriteRenderer.flipX = true;
+        }
+        if (xInput < 0)
+        {
+            spriteRenderer.flipX = false;
         }
     }
 }
